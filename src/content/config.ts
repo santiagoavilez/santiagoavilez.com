23f071eceb43a263a81dae4c64eb01d0aa0fabd8
santiagoavilez <santiagolingreed@gmail.com>
2023-02-10 00:36:34 -0300

    trabajos realizados: laborar, uncoactiva

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,12 +3,21 @@ import { defineCollection, z } from 'astro:content';
 export const collections = {
 	work: defineCollection({
 		schema: z.object({
+
 			title: z.string(),
 			description: z.string(),
 			publishDate: z.coerce.date(),
 			tags: z.array(z.string()),
+			tools: z.array(z.object({
+				name: z.string(),
+				alt: z.string().optional(),
+				icon: z.string(),
+			})).optional(),
+			live: z.string().optional(),
+			git: z.string().optional(),
 			img: z.string(),
 			img_alt: z.string().optional(),
+
 		}),
 	}),
 };

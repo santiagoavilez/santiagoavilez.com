bdd0eaaf79cdb119cac1cc0b2f85019332379071
santiagoavilez <santiagolingreed@gmail.com>
2023-03-01 17:47:51 -0300

    cambio en logo y colores de la marca

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,22 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class', // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'cerulean': {
+					'50': '#ecfeff',
+					'100': '#cefaff',
+					'200': '#a3f4fe',
+					'300': '#63e9fd',
+					'400': '#1dd4f3',
+					'500': '#01b7da',
+					'600': '#0491b6',
+					'700': '#0b7493',
+					'800': '#135d77',
+					'900': '#144e65',
+				},
+			}
+		},
 	},
 	plugins: [],
 }

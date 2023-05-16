5258d32cf970351a930ceec330ede1549ee72798
Santiago Avilez <90413630+santiagoavilez@users.noreply.github.com>
2023-05-16 14:26:23 +0000

    fix work to projects content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,46 +1,50 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 export const collections = {
-	work: defineCollection({
-		schema: z.object({
-
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			tools: z.array(z.object({
-				name: z.string(),
-				alt: z.string().optional(),
-				icon: z.string().optional(),
-			})).optional(),
-			live: z.string().optional(),
-			git: z.string().optional(),
-			img: z.string(),
-			img_w: z.number(),
-			img_h: z.number(),
-			img_alt: z.string().optional(),
-
-		}),
-	}),
-	blog: defineCollection({
-		schema: z.object({
-
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			tools: z.array(z.object({
-				name: z.string(),
-				alt: z.string().optional(),
-				icon: z.string().optional(),
-			})).optional(),
-			live: z.string().optional(),
-			git: z.string().optional(),
-			img: z.string(),
-			img_w: z.number(),
-			img_h: z.number(),
-			img_alt: z.string().optional(),
-
-		}),
-	}),
+  projects: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      publishDate: z.coerce.date(),
+      tags: z.array(z.string()),
+      tools: z
+        .array(
+          z.object({
+            name: z.string(),
+            alt: z.string().optional(),
+            icon: z.string().optional(),
+          })
+        )
+        .optional(),
+      live: z.string().optional(),
+      git: z.string().optional(),
+      img: z.string(),
+      img_w: z.number(),
+      img_h: z.number(),
+      img_alt: z.string().optional(),
+    }),
+  }),
+  blog: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      publishDate: z.coerce.date(),
+      tags: z.array(z.string()),
+      tools: z
+        .array(
+          z.object({
+            name: z.string(),
+            alt: z.string().optional(),
+            icon: z.string().optional(),
+          })
+        )
+        .optional(),
+      live: z.string().optional(),
+      git: z.string().optional(),
+      img: z.string(),
+      img_w: z.number(),
+      img_h: z.number(),
+      img_alt: z.string().optional(),
+    }),
+  }),
 };

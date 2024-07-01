1df859edd74a6d4a1fb0fa091af678b074f51e46
santiagoavilez <santiagolingreed@gmail.com>
2024-07-01 15:53:13 -0300

    chore: Update image paths in Nav and Hero components, remove ThemeToggle component

--- src/components/react/animated-cta.tsx
+++ src/components/react/animated-cta.tsx
@@ -5,47 +5,75 @@ export interface IanimatedCta {
         text: string;
         highlight?: boolean;
     }[]
+    isDeleting?: boolean;
 }
 
-const AnimatedCta = ({ arrayText }: IanimatedCta) => {
+const AnimatedCta = ({ arrayText , isDeleting }: IanimatedCta) => {
     const [index, setIndex] = useState(0);
     const [displayText, setDisplayText] = useState<JSX.Element[]>([]);
     const [typingSymbolVisible, setTypingSymbolVisible] = useState(true);
 
 
 
-    useEffect(() => {
-        let i = 0; //index for the current word
-        const typingInterval = setInterval(() => {
-            if (i < arrayText[index].text.length) {
-                setDisplayText((prev) => [
-                    ...prev.filter((_, idx) => idx !== prev.length - 1), // Remove the last element (typing symbol) before adding a new letter
-                    <span key={`${index}-${i}`} className={arrayText[index].highlight ? 'dark:text-[#3E88CF] text-[#3E88CF]/70 ' : ''}>
-                        {arrayText[index].text[i]}
-                    </span>,
-                    <span key={`${index}-typing-symbol`} className="typing-symbol animate-pulse dark:text-gray-50 text-gray-900">|  </span> // Add the typing symbol at the end
-                ]);
-                i++;
-            } else {
-                clearInterval(typingInterval);
-                // wait for a while and then start the next word
-                setDisplayText((prev) => prev.filter((_, idx) => idx !== prev.length - 1)); // Remove the typing symbol after finishing typing
 
-                setTimeout(() => {
+    useEffect(() => {
+        let i = 0; // index for the current character
+        let typingInterval;
+
+        if (!isDeleting) {
+            typingInterval = setInterval(() => {
+                if (i < arrayText[index].text.length) {
+                    setDisplayText((prev) => [
+                        ...prev.filter((_, idx) => idx !== prev.length - 1), // Remove the last element (typing symbol) before adding a new letter
+                        <span key={`${index}-${i}`} className={arrayText[index].highlight ? 'text-cerulean-400  ' : ''}>
+                            {arrayText[index].text[i]}
+                        </span>,
+                        <span key={`${index}-typing-symbol`} className="typing-symbol animate-pulse dark:text-gray-50 text-gray-900">|  </span> // Add the typing symbol at the end
+                    ]);
+                    i++;
+                } else {
+                    clearInterval(typingInterval);
+                    // wait for a while and then start the next word
+                    setDisplayText((prev) => prev.filter((_, idx) => idx !== prev.length - 1)); // Remove the typing symbol after finishing typing
+
+                    setTimeout(() => {
+                        if (index < arrayText.length - 1) {
+                            setIndex(index + 1); // update the index to the next word
+                        } else {
+                            // optional: restart the animation
+                            // setIndex(0);
+                            // setDisplayText([]); // clean the display text
+
+
+                        }// reset the index for the next word
+                    }, 35); // adjust this value to control the speed of the animation
+                }
+            }, 20); // adjust this value to control the speed of the animation
+
+            return () => clearInterval(typingInterval); // adjust this value to control the speed of the animation
+        } else {
+            typingInterval = setInterval(() => {
+                if (displayText.length > 0) {
+                    setDisplayText((prev) => prev.slice(0, -2));
+
+                    setDisplayText((prev) => [
+                        ...prev, // Remove the last element (typing symbol) before adding a new letter
+
+                        <span key={`${index}-typing-symbol`} className="typing-symbol animate-pulse dark:text-gray-50 text-gray-900">|  </span> // Add the typing symbol at the end
+                    ]);
+                } else {
+                    clearInterval(typingInterval);
                     if (index < arrayText.length - 1) {
-                        setIndex(index + 1); // update the index to the next word
+                        setIndex(index + 1); // Move to the next word
                     } else {
-                        // optional: restart the animation
-                        // setIndex(0);
-                        // setDisplayText([]); // clean the display text
+                        setIndex(0); // Restart from the first word
                     }
-                    i = 0; // reset the index for the next word
-                }, 35); // adjust this value to control the speed of the animation
-            }
-        }, 35); // adjust this value to control the speed of the animation
+                }
+            }, 25); // adjust this value for deletion speed
+        }
 
         return () => clearInterval(typingInterval); // cleanup the interval
-    }, [index,  arrayText]);
+    }, [index, arrayText, isDeleting]);
     return (
         <span className="animate-bg-animation">
             {displayText}
@@ -54,3 +82,5 @@ const AnimatedCta = ({ arrayText }: IanimatedCta) => {
 };
 
 export default AnimatedCta;
+
+

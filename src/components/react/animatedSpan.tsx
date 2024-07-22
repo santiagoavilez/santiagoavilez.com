7207e530634106be1a5074055adbab921b960fd4
santiagoavilez <santiagolingreed@gmail.com>
2024-07-22 14:11:04 -0300

    chore: Update image paths, remove unused components, and update static text for SEO in index page

--- src/components/react/animatedSpan.tsx
+++ src/components/react/animatedSpan.tsx
@@ -20,7 +20,7 @@ const AnimatedSpan = ({ arrayText }: { arrayText: string[] }) => {
     }, []);
 
     return (
-        <span data-state={isOpen ? 'open' : 'closed'} key={index} className={` animate-bg-animation bg-gradient-to-r   from-purple-500 via-yellow-500  to-cyan-500 bg-[500%,500%] bg-clip-text bg-no-repeat font-bold text-transparent duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]`}>{arrayText[index]}</span>
+        <span data-state={isOpen ? 'open' : 'closed'} key={index} className={` animate-bg-animation bg-gradient-to-r    from-purple-500 via-yellow-500  to-cyan-500 bg-[500%,500%] bg-clip-text bg-no-repeat font-bold text-transparent `}>{arrayText[index]}</span>
 
     );
 };
